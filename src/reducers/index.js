3ce79142c2374ce074e06ecd7412a0a7d4a912b2
wagnersantos <wagtvc@gmail.com>
2019-01-04 10:33:36 -0200

    mudando o estado de articles

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
+impoort {ADD_ARTICLE} from '../constatns/actions-types';
+
 const InitialState = {
 	articles: []
 }
 
 function RootReducer(state = InitialState, action){
+	if(action.type === ADD_ARTICLE){
+			return object.assign({},{
+				articles: state.articles.concat(action.payload);
+			});
+	}
 	return state;
 }
 
